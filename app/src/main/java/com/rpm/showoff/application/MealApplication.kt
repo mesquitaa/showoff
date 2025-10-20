60a386f9c1427a5a320ca6a57150c32a50264eab
Ruy Mesquita <ruymesquitaneto@gmail.com>
2025-10-20 15:21:29 +0100

    feat: Implement MealApplication class and update AndroidManifest for application entry point

---
+++ app/src/main/java/com/rpm/showoff/application/MealApplication.kt
@@ -0,0 +1,20 @@
+package com.rpm.showoff.application
+
+import android.app.Application
+import com.rpm.category.list.di.categoryListModule
+import com.rpm.core.network.di.networkModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MealApplication : Application() {
+  override fun onCreate() {
+    super.onCreate()
+
+    startKoin {
+      androidLogger()
+      androidContext(this@MealApplication)
+      modules(networkModule, categoryListModule)
+    }
+  }
+}
--- app/src/main/java/com/rpm/showoff/navigation/MealNavigation.kt
+++ app/src/main/java/com/rpm/showoff/navigation/MealNavigation.kt
@@ -6,6 +6,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.rpm.category.list.ui.CategoryListScreen
 
 @Composable
 fun MealNavigation(navController: NavHostController = rememberNavController()) {
