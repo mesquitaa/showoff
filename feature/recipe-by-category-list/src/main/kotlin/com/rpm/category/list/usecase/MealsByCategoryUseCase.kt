24e8dda0b21c9a5b2be8ef0aaf3ff772dc0d4be2
Ruy Mesquita <ruymesquitaneto@gmail.com>
2025-10-20 22:50:16 +0100

    feat: Add MealsByCategoryUseCase and corresponding tests for fetching meals by category

---
+++ feature/recipe-by-category-list/src/main/kotlin/com/rpm/category/list/usecase/MealsByCategoryUseCase.kt
@@ -0,0 +1,19 @@
+package com.rpm.category.list.usecase
+
+import com.rpm.core.domain.entity.Meal
+import com.rpm.core.domain.repository.MealByCategoryRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+
+class MealsByCategoryUseCase(
+  private val repository: MealByCategoryRepository,
+) {
+  suspend operator fun invoke(category: String): Flow<Result<List<Meal>?>> = flow {
+    repository.getMealsByCategory(category).collect { response ->
+      emit(Result.success(response))
+    }
+  }.catch { e ->
+    emit(Result.failure(e))
+  }
+}
---
+++ feature/recipe-by-category-list/src/test/kotlin/com/rpm/category/list/usecase/MealsByCategoryUseCaseTest.kt
@@ -0,0 +1,48 @@
+package com.rpm.category.list.usecase
+
+import com.rpm.core.domain.entity.Category
+import com.rpm.core.domain.entity.Meal
+import com.rpm.core.domain.repository.MealByCategoryRepository
+import com.rpm.core.domain.repository.MealCategoryRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.single
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class MealsByCategoryUseCaseTest {
+  private val mockRepository = mockk<MealByCategoryRepository>(relaxed = true)
+  private val subject by lazy { MealsByCategoryUseCase(mockRepository) }
+
+  @Test
+  fun `invoke when repository returns success then should emit success`() = runTest {
+    // GIVEN
+    val mockList = listOf(Meal(1, "meal", "url"))
+    coEvery { mockRepository.getMealsByCategory(FAKE_CATEGORY) } returns flowOf(mockList)
+
+    // WHEN
+    val resultFlow = subject.invoke(FAKE_CATEGORY)
+
+    // THEN
+    assertEquals(Result.success(mockList), resultFlow.single())
+  }
+
+  @Test
+  fun `invoke when repository throws exception then should emit failure`() = runTest {
+    // GIVEN
+    val sutException = Exception("Error")
+    coEvery { mockRepository.getMealsByCategory(FAKE_CATEGORY) } throws sutException
+
+    // WHEN
+    val resultFlow = subject.invoke(FAKE_CATEGORY)
+
+    // THEN
+    assertEquals(Result.failure<Exception>(sutException), resultFlow.single())
+  }
+
+  companion object {
+    private const val FAKE_CATEGORY = "FAKE_CATEGORY"
+  }
+}
