982252ddfa44385f4ad42e95b6ee488e6ab0f6bf
Ruy Mesquita <ruymesquitaneto@gmail.com>
2025-10-20 21:55:07 +0100

    Refactor: Apply code style formatting

--- feature/category-list/src/main/kotlin/com/rpm/category/list/state/CategoryListUiAction.kt
+++ feature/category-list/src/main/kotlin/com/rpm/category/list/state/CategoryListUiAction.kt
@@ -3,9 +3,9 @@ package com.rpm.category.list.state
 import com.rpm.core.domain.entity.Category
 
 sealed class CategoryListUiAction {
-    object LoadData : CategoryListUiAction()
+  object LoadData : CategoryListUiAction()
 
-    data class SelectCategory(
-      val category: Category,
-    ) : CategoryListUiAction()
+  data class SelectCategory(
+    val category: Category,
+  ) : CategoryListUiAction()
 }
--- feature/category-list/src/main/kotlin/com/rpm/category/list/state/CategoryListUiEffect.kt
+++ feature/category-list/src/main/kotlin/com/rpm/category/list/state/CategoryListUiEffect.kt
@@ -1,7 +1,7 @@
 package com.rpm.category.list.state
 
 sealed class CategoryListUiEffect {
-    data class NavigateToCategoryList(
-        val category: String,
-    ) : CategoryListUiEffect()
+  data class NavigateToCategoryList(
+    val category: String,
+  ) : CategoryListUiEffect()
 }
